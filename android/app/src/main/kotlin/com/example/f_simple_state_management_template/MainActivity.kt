9b797fef79d5714afdf56fdb3a795f3b3469113a
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2025-08-20 07:56:27 -0500

    update android to 202530

--- android/app/src/main/kotlin/com/example/f_simple_state_management_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/f_simple_state_management_template/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.f_simple_state_management_template
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
